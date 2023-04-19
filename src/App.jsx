86fba73cc16c0ee3d4ac275bb79681259178d74d
Joaquin Cardoso Rios <105686879+joaquincardosorios@users.noreply.github.com>
2023-04-19 16:51:13 -0400

    set-range-input

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ const App = () => {
   const [ cantidad, setCantidad ] = useState(2000000);
 
 
+  function handleChange(e) {
+    setCantidad(+e.target.value)
+  }
+
   return ( 
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
@@ -13,7 +17,9 @@ const App = () => {
       <input 
         type="range"
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={ handleChange }
       />
+      <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>{cantidad}</p>
     </div>
   );
 }
