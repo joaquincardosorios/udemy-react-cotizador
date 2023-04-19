49a04c52720f63601bc987154914c769939824dd
Joaquin Cardoso Rios <105686879+joaquincardosorios@users.noreply.github.com>
2023-04-19 16:39:22 -0400

    first-steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-function App() {
-  return (
-    <h1>Hola Mundo</h1>
-  )
-}
+import { useState } from 'react'
+import Header from "./components/Header";
 
-export default App
+
+const App = () => {
+  const [ cantidad, setCantidad ] = useState(2000000);
+
+
+  return ( 
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      <Header />
+
+      <input 
+        type="range"
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+      />
+    </div>
+  );
+}
+ 
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+function Header() {
+    return(
+        <h1 className="text-4xl font-extrabold text-gray-500 text-center">
+            ¿Cuando <span className="text-indigo-600">dinero </span> necesitas?
+        </h1>
+    )
+}
+
+export default Header;
